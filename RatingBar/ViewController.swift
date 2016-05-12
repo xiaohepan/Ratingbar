c8a4ae4f5347666d5f27b5b11bed18162ed9ec25
xiaohepan <474876109@qq.com>
2016-05-12 15:11:27 +0800

    Initial Commit

---
+++ RatingBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RatingBar
+//
+//  Created by panxiaohe on 16/5/12.
+//  Copyright © 2016年 panxiaohe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
